2e09dd28c72988841d9b59e3a420e3db4d6e374a
SFiterman <ing.solfiterman@gmail.com>
2022-09-24 14:20:28 -0300

    Funciona???

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,11 @@
 #include <cstdlib>
 #include <iostream>
-
 #include "example.h"
 
 int main(int argc, char** argv)
 {
     foobar::Example example{};
     std::cout << "Example.getValue() => " << example.getValue() << std::endl;
-
+    
     return EXIT_SUCCESS;
 }
